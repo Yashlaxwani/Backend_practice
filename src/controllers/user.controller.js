19301f6e40ea86158c24bb6f191d6a835d88f507
Yashlaxwani <yashlaxwani@gamil.com>
2024-07-27 14:41:27 +0530

    updating controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,7 +32,7 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-    //console.log(req.files);
+
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
