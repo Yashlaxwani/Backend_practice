d7cd5339035449adad1d3f9c8b2a689d57832f6f
Yashlaxwani <yashlaxwani@gamil.com>
2024-07-28 00:04:52 +0530

    logout and login feature

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -18,5 +18,10 @@ router.route("/register").post(
     registerUser
     )
 
+    router.route("/login").post(loginUser)
+
+    //secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+
 
 export default router
